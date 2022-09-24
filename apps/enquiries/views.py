0cc47b6cd3d6b83db961ee2a62369b69ca43e00c
Daniel <danydu1299@gmail.com>
2022-09-24 16:05:48 -0500

    Enpoints to enquiries and ratings
    Done :heavy_check_mark:
    Ratings: :new_moon_with_face:
        - Description: Rating a proflie
        - Enpoint: api/v1/ratings/id_profile
        - Method: POST
    
    Enquiries: :new_moon_with_face:
        - Description: Create an enquiry Send an email (use mailtrap)
        - Enpoint: api/v1/enquiries/
        - Method: POST
    
    To do :heavy_plus_sign:
        - Dockerize the project :whale:
        - Use Nginx as a reverse proxy

--- apps/enquiries/views.py
+++ apps/enquiries/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
 
-# Create your views here.
+from real_estate.settings.development import DEFAULT_FROM_EMAIL
+from rest_framework import permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+
+from .models import Enquiry
+
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def send_enquiry_email(request):
+    """
+    ----------------------------
+    Send an enquiry email to an agent
+    ----------------------------
+    """
+    data = request.data
+    try:
+        name = data["name"]
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        from_email = data["email"]
+        recipient_list = [DEFAULT_FROM_EMAIL]
+
+        send_mail(subject, message, from_email, recipient_list, fail_silently=True)
+        enquiry = Enquiry(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message
+        )
+        enquiry.save()
+
+        return Response({"success"": Your Enquiry was succesfully submitted"}, status=status.HTTP_200_OK)
+    except Exception as err:
+        # raise err
+        return Response({"fail": "Your Enquiry was not submitted"}, status=status.HTTP_400_BAD_REQUEST)
--- apps/ratings/views.py
+++ apps/ratings/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+"""
+-------------------------
+Views to Profiles models
+-------------------------
+"""
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+# My models and serializers
+from apps.profiles.models import Profile
+from .models import Rating
+
+User = get_user_model()
+
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def create_agent_review(request, profile_id):
+    """
+    ----------------------------
+    Create a review for an agent
+    ----------------------------
+    """
+    agent_profile = Profile.objects.get(id=profile_id)
+    data = request.data
+
+    profile_user = User.objects.get(pkid=agent_profile.user.pkid)
+    if profile_user.email == request.user.email:
+        format_response = {'message': 'You cannot review yourself'}
+        return Response(format_response, status=status.HTTP_403_FORBIDDEN)
+
+    alreadyExists = agent_profile.agent_review.filter(agent__pkid=profile_user.pkid).exists()
+
+    if alreadyExists:
+        format_response = {'detail': 'Profile already reviewed'}
+        return Response(format_response, status=status.HTTP_400_BAD_REQUEST)
+    elif data["rating"] == 0:
+        format_response = {'detail': 'Please select a rating'}
+        return Response(format_response, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        review = Rating.objects.create(
+            rater=request.user,
+            agent=agent_profile,
+            rating=data["rating"],
+            comment=data["comment"]
+        )
+        reviews = agent_profile.agent_review.all()
+        agent_profile.num_reviews = len(reviews)
+
+        total = 0
+        for review in reviews:
+            total += review.rating
+
+        return Response("Review Added", status=status.HTTP_200_OK)
