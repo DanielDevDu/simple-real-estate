fadd1dd18d6cc74bd58422d31f7e5f2350191efc
Daniel <danydu1299@gmail.com>
2022-11-19 16:34:41 -0500

    feature: Setup readux-toolkit to load properties

--- apps/properties/serializers.py
+++ apps/properties/serializers.py
@@ -20,6 +20,12 @@ class PropertySerializer(serializers.ModelSerializer):
 
     country = CountryField()
     user = serializers.SerializerMethodField()
+    cover_photo = serializers.SerializerMethodField()
+    profile_photo = serializers.SerializerMethodField()
+    photo1 = serializers.SerializerMethodField()
+    photo2 = serializers.SerializerMethodField()
+    photo3 = serializers.SerializerMethodField()
+    photo4 = serializers.SerializerMethodField()
 
     class Meta:
         model = Property
@@ -33,6 +39,24 @@ class PropertySerializer(serializers.ModelSerializer):
         ----------------------
         """
         return obj.user.username
+
+    def get_cover_photo(self, obj):
+        return obj.cover_photo.url
+
+    def get_photo1(self, obj):
+        return obj.photo1.url
+
+    def get_photo2(self, obj):
+        return obj.photo2.url
+
+    def get_photo3(self, obj):
+        return obj.photo3.url
+
+    def get_photo4(self, obj):
+        return obj.photo4.url
+
+    def get_profile_photo(self, obj):
+        return obj.user.profile.profile_photo.url
 
 
 class PropertyCreateSerializer(serializers.ModelSerializer):
--- client/src/pages/PropertiesPage.jsx
+++ client/src/pages/PropertiesPage.jsx
@@ -1,7 +1,24 @@
 import { Col, Container, Row } from 'react-bootstrap';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Spinner from '../components/Spinner';
+import { getProperties } from '../features/properties/propertySlice';
 
 const PropertiesPage = () => {
+  const { propertiesList, isLoading } = useSelector(
+    (state) => state.propertiesList
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProperties());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <>
       <Container>
